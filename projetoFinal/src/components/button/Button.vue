85dc924f2c684f9bef95908ec1051c4b84d6112a
gustavo <gustavo_elias-souza@estudante.sesisenai.org.br>
2024-06-11 08:35:27 -0300

    add bootstarp vue

--- projetoFinal/src/components/button/Button.vue
+++ projetoFinal/src/components/button/Button.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import type {ButtonProps} from "@/components/button/types";
+import type { ButtonProps } from '@/components/button/types'
 
 const props = defineProps<ButtonProps>()
 </script>
 <template>
-  <input :type="props.type" :value="props.value">
+  <input :type="props.type" :value="props.value" />
 </template>
---
+++ projetoFinal/src/router/index.ts
@@ -0,0 +1,15 @@
+// import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
+
+// const routes: RouteRecordRaw[] = [
+//   {
+//     path: "/",
+//     // component: 
+//   }
+// ]
+
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes
+// })
+
+// export default router
